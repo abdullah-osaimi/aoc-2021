b7863263fa1b6fa648e499af09a024decd754e3d
Abdullah AlOsaimi <osaimiacc@gmail.com>
2021-12-05 15:26:36 +0300

    add day3

--- day3/p2.js
+++ day3/p2.js
@@ -4,26 +4,65 @@ const filename = 'input.txt'
 const file = fs.readFileSync(filename).toString('utf8')
 const numbers = file.split('\n').slice(0, -1)
 
-let numberWithOne = [];
-let numberWithZero = [];
-
-for (let i = 0; i < numbers[0].split('').length; i++)
-{
-    // first bit of each number
-    console.log('bit number', i);
-    let sum = 0;
-    for (let j = 0; j < numbers.length; j++) {
-        let number = numbers[j].split('')[i]
-        if (number == '1') {
-            sum++;
-            numberWithOne.push(number)
-        } else {
-            numberWithZero.push(number)
-        }
-    }
-    console.log(`the number of 1's in the ${i} bit is ${sum}`)
-    sum = 0;
-}
-
 
 // lifeSupportRating = oxygen * co2
+let bit = 0;
+let numberWithOne = [];
+let numberWithZero = [];
+let oxygenRating = [...numbers];
+let co2Rating = [...numbers];
+
+while (oxygenRating.length != 1 || co2Rating.length != 1) {
+
+
+    if (oxygenRating.length != 1) {
+        for (let i = 0; i < oxygenRating.length; i++) {
+            let number = oxygenRating[i].split('')[bit]
+
+            if (number == '1') {
+                numberWithOne.push(oxygenRating[i])
+            } else {
+                numberWithZero.push(oxygenRating[i])
+            }
+        }
+
+        if (numberWithOne.length >= numberWithZero.length) {
+            oxygenRating = [...numberWithOne];
+        } else {
+            oxygenRating = [...numberWithZero];
+        }
+
+        numberWithOne = []
+        numberWithZero = []
+    }
+
+
+
+
+    if (co2Rating.length != 1) {
+        for (let i = 0; i < co2Rating.length; i++) {
+            let number = co2Rating[i].split('')[bit]
+
+            if (number == '1') {
+                numberWithOne.push(co2Rating[i])
+            } else {
+                numberWithZero.push(co2Rating[i])
+            }
+        }
+
+        if (numberWithOne.length >= numberWithZero.length) {
+            co2Rating = [...numberWithZero];
+        } else {
+            co2Rating = [...numberWithOne];
+        }
+
+        numberWithOne = []
+        numberWithZero = []
+
+    }
+
+    bit++;
+}
+
+const lifeRating = parseInt(co2Rating[0], 2) * parseInt(oxygenRating[0], 2);
+console.log(lifeRating)
