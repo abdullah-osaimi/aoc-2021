93e1a306cf411d60ed2767478cb0bb26d7f50b8e
Abdullah AlOsaimi <osaimiacc@gmail.com>
2021-12-08 18:10:00 +0300

    refactoring p1

--- day4/p1.js
+++ day4/p1.js
@@ -14,30 +14,48 @@ boards.forEach((board, boardIndex) => {
 })
 
 
-let board = boards[0]
-let boardWithAsterisk = board.map(element => element.slice())
-let currentDrawnNumberIndex = 0
+let bingoo = false
 
-while (currentDrawnNumberIndex < drawnNumbers.length) {
+for (let numberIndex = 0; numberIndex < drawnNumbers.length && !bingoo; numberIndex++) {
 
-    // test each board after drawing a number
-    // testing with the first board only
-    for (let row = 0; row < boardWithAsterisk.length; row++) 
-    {
-        for (let column = 0; column < boardWithAsterisk[row].length; column++)
-        {
-            if (drawnNumbers[currentDrawnNumberIndex] == board[row][column]) {
-                boardWithAsterisk[row][column] = '*'
-            }
-        }
+
+    for (let boardIndex = 0; boardIndex < boards.length; boardIndex++) {
+        let currentBoard = boards[boardIndex]
+        let currentCalledNumber = drawnNumbers[numberIndex]
+
+        markBoard(currentBoard, currentCalledNumber)
+
+        let result = checkIfBingo(currentBoard)
+        if (result.bingo) {
+            bingoo = true
+
+            result.score = calculateScore(currentBoard, currentCalledNumber)
+            result.boardIndex = boardIndex
+            showResult(result)
+
+            break
+        } 
     }
-
-    currentDrawnNumberIndex++;
-    checkIfBingo(boardWithAsterisk)
 }
 
 
+function markBoard(board, number) {
+    for (let row = 0; row < board.length; row++) 
+    {
+        for (let column = 0; column < board[row].length; column++)
+        {
+            if (number == board[row][column]) {
+                board[row][column] = '*'
+            }
+        }
+    }
+}
+
 function checkIfBingo(board) {
+
+    let result = {
+        bingo: false,
+    }
 
     //check if row starts with an *
     for (let row = 0; row < board.length; row++) 
@@ -50,7 +68,9 @@ function checkIfBingo(board) {
         }
 
         if (column == board[row].length && board[row].at(-1) == "*") {
-            console.log(`bingoooo row: ${row}`)
+            result.bingo = true;
+            result.row = row;
+            return result
         }
     }
 
@@ -66,7 +86,34 @@ function checkIfBingo(board) {
 
         if (row == board.length && board.at(-1)[column] == "*") 
         {
-            console.log(`bingoo at column ${column}`)
+            result.bingo = true;
+            result.column = column;
+            return result
         }
     }
+
+
+    return result;
+}
+
+function calculateScore(board, lastCalledNumber) {
+    let sum = 0
+
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[i].length; j++) {
+            if (board[i][j] != '*')
+                sum += parseInt(board[i][j])
+        }
+    }
+
+    return sum * lastCalledNumber;
+}
+
+function showResult(result) {
+    if ('row' in result)
+        console.log(`bingooo at row ${result.row}, board ${result.boardIndex}`);
+    else
+        console.log(`bingooo at column  ${result.column}, board ${result.boardIndex}`);
+
+    console.log(`your score is ${result.score}`);
 }
